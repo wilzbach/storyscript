96d63d1d83685da5faddd707cb97f200c6359a8b
Steve Peak <steve@stevepeak.net>
2017-12-31 13:58:27 +0100

    remove redundant test context

--- tests/unittests/test_expressions.py
+++ tests/unittests/test_expressions.py
@@ -6,24 +6,24 @@ import storyscript
 
 
 @pytest.mark.parametrize('script,args', [
-    ('if x\n\tpass', [{'$OBJECT': 'path', 'paths': ['x']}]),
-    ('if true\n\tpass', [True]),
-    ('if false\n\tpass', [False]),
+    ('if x', [{'$OBJECT': 'path', 'paths': ['x']}]),
+    ('if true', [True]),
+    ('if false', [False]),
 ])
 def test_bool(script, args):
-    story = storyscript.parse(script).json()
+    story = storyscript.parse(script + '\n\tpass').json()
     print(dumps(story['script'], indent=2))
     assert story['script']['1']['args'] == args
 
 
 @pytest.mark.parametrize('script,expression', [
-    ('if 1=2\n\tpass', '1 == 2'),
-    ('if 1 == 2\n\tpass', '1 == 2'),
-    ('if 1 is equal to 2\n\tpass', '1 == 2'),
-    ('if 1 equals 2\n\tpass', '1 == 2'),
+    ('if 1=2', '1 == 2'),
+    ('if 1 == 2', '1 == 2'),
+    ('if 1 is equal to 2', '1 == 2'),
+    ('if 1 equals 2', '1 == 2'),
 ])
 def test_eq(script, expression):
-    story = storyscript.parse(script).json()
+    story = storyscript.parse(script + '\n\tpass').json()
     print(dumps(story['script'], indent=2))
     assert story['script']['1']['args'] == [
         {
@@ -34,13 +34,36 @@ def test_eq(script, expression):
     ]
 
 
+@pytest.mark.parametrize('script,method', [
+    ('if abc like /apple/', 'like'),
+    ('if abc is like /apple/', 'like'),
+    ('if abc isnt like /apple/', 'notlike'),
+])
+def test_regexp(script, method):
+    story = storyscript.parse(script + '\n\tpass').json()
+    print(dumps(story['script'], indent=2))
+    assert story['script']['1']['args'] == [
+        {
+            '$OBJECT': 'method',
+            'method': method,
+            'left': {
+                '$OBJECT': 'path',
+                'paths': ['abc']
+            },
+            'right': {
+                '$OBJECT': 'regexp',
                 'regexp': 'apple'
+            }
+        }
+    ]
+
+
 @pytest.mark.parametrize('script,expression', [
-    ('if x and y\n\tpass', '{} and {}'),
-    ('if x or y\n\tpass', '{} or {}')
+    ('if x and y', '{} and {}'),
+    ('if x or y', '{} or {}')
 ])
 def test_and_or(script, expression):
-    story = storyscript.parse(script).json()
+    story = storyscript.parse(script + '\n\tpass').json()
     print(dumps(story['script'], indent=2))
     assert story['script']['1']['args'] == [
         {
@@ -55,11 +78,11 @@ def test_and_or(script, expression):
 
 
 @pytest.mark.parametrize('script,expression', [
-    ('if (x and y) or z\n\tpass', '( {} and {} ) or {}'),
-    ('if x or (y and z)\n\tpass', '{} or ( {} and {} )')
+    ('if (x and y) or z', '( {} and {} ) or {}'),
+    ('if x or (y and z)', '{} or ( {} and {} )')
 ])
 def test_group(script, expression):
-    story = storyscript.parse(script).json()
+    story = storyscript.parse(script + '\n\tpass').json()
     print(dumps(story['script'], indent=2))
     assert story['script']['1']['args'] == [
         {
@@ -75,21 +98,21 @@ def test_group(script, expression):
 
 
 @pytest.mark.parametrize('script,expression', [
-    ('if 1>2\n\tpass', '1 > 2'),
-    ('if 1 > 2\n\tpass', '1 > 2'),
-    ('if 1 is greater then 2\n\tpass', '1 > 2'),
-    ('if 1 is greater than or equal to 2\n\tpass', '1 >= 2'),
-    ('if 1>=2\n\tpass', '1 >= 2'),
-    ('if 1 >= 2\n\tpass', '1 >= 2'),
-    ('if 1<2\n\tpass', '1 < 2'),
-    ('if 1 < 2\n\tpass', '1 < 2'),
-    ('if 1 is less than 2\n\tpass', '1 < 2'),
-    ('if 1 is less then 2\n\tpass', '1 < 2'),
-    ('if 1 less than or equal to 2\n\tpass', '1 <= 2'),
+    ('if 1>2', '1 > 2'),
+    ('if 1 > 2', '1 > 2'),
+    ('if 1 is greater then 2', '1 > 2'),
+    ('if 1 is greater than or equal to 2', '1 >= 2'),
+    ('if 1>=2', '1 >= 2'),
+    ('if 1 >= 2', '1 >= 2'),
+    ('if 1<2', '1 < 2'),
+    ('if 1 < 2', '1 < 2'),
+    ('if 1 is less than 2', '1 < 2'),
+    ('if 1 is less then 2', '1 < 2'),
+    ('if 1 less than or equal to 2', '1 <= 2'),
 
 ])
 def test_gtlt(script, expression):
-    story = storyscript.parse(script).json()
+    story = storyscript.parse(script + '\n\tpass').json()
     print(dumps(story['script'], indent=2))
     assert story['script']['1']['args'] == [
         {
