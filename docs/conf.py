92eef9500186c585d3c36722b9d7cb00bee5eaac
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-23 13:44:06 +0000

    docs(conf): use guzzle theme

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import guzzle_sphinx_theme
 
 
 # -- General configuration ------------------------------------------------
@@ -31,7 +32,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['guzzle_sphinx_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -83,13 +84,18 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme_path = guzzle_sphinx_theme.html_theme_path()
+html_theme = 'guzzle_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    "project_nav_name": "Storyscript"
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -164,6 +170,3 @@ texinfo_documents = [
      author, 'Storyscript', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
