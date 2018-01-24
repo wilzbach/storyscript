09bd067152424fcea60a0a92394eaabf5f6f22c7
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-24 16:33:16 +0000

    feat(path): add storyscript.tree.Path module

--- storyscript/tree/__init__.py
+++ storyscript/tree/__init__.py
@@ -1,6 +1,7 @@
 from .Comparison import Comparison
 from .Method import Method
+from .Path import Path
 from .String import String
 
 
-__all__ = ['Comparison', 'Method', 'String']
+__all__ = ['Comparison', 'Method', 'Path', 'String']
---
+++ tests/unittests/tree/test_path.py
@@ -0,0 +1,22 @@
+from pytest import fixture
+
+from storyscript.tree import Path
+
+
+@fixture
+def path():
+    return Path('parser', 'line_number', 'path')
+
+
+def test_path_init(path):
+    assert path.parser == 'parser'
+    assert path.lineno == 'line_number'
+    assert path.paths == 'path'
+
+
+def test_path_split(path):
+    assert path.split("a.b['c']") == ['a', 'b', 'c']
+
+
+def test_path_json(path):
+    assert path.json() == {'$OBJECT': 'path', 'paths': path.paths}
