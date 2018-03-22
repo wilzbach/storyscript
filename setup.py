12cece4670a869b52a0943c906844fd375f494b3
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-03-22 14:40:47 +0000

    chore(setup): load version from storyscript.version

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
 from setuptools.command.install import install
+from storyscript.version import version
 
 
 classifiers = [
@@ -36,7 +37,7 @@ class CustomInstallCommand(install):
 
 
 setup(name='storyscript',
-      version='0.0.8',
+      version=version,
       description='',
       long_description='',
       classifiers=classifiers,
