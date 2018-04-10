140fabb664972d00f3986f1b1b1cb95f2b13495d
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-04-10 15:17:45 +0100

    feat(parser): add Parser.grammar method

--- storyscript/parser.py
+++ storyscript/parser.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from .grammar import Grammar
 
 
 class Parser:
@@ -6,3 +7,8 @@ class Parser:
     def __init__(self, source, algo='lalr'):
         self.source = source
         self.algo = algo
+
+    def grammar(self):
+        grammar = Grammar()
+        grammar.start('line')
+        return grammar.build()
--- tests/unittests/parser.py
+++ tests/unittests/parser.py
@@ -11,3 +11,13 @@ def test_parser_init():
 def test_parser_init_algo():
     parser = Parser('source', algo='algo')
     assert parser.algo == 'algo'
+
+
+def test_parser_grammar(patch):
+    patch.init(Grammar)
+    patch.many(Grammar, ['start', 'rule', 'terminal', 'ignore', 'load',
+                         'build'])
+    result = Parser('source').grammar()
+    Grammar.start.assert_called_with('line')
+    assert Grammar.build.call_count == 1
+    assert result == Grammar.build()
