c82aa4131be23b9634318cc504cb65b1c837f9db
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-05-04 18:11:41 +0100

    feat(transformer): add Transformer.start method

--- storyscript/parser/transformer.py
+++ storyscript/parser/transformer.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from lark import Transformer as LarkTransformer
 
+from .tree import Tree
+
 
 class Transformer(LarkTransformer):
+
+    def start(self, matches):
+        return Tree('line', matches)
 
     def string(self, matches):
         return matches
--- tests/unittests/parser/transformer.py
+++ tests/unittests/parser/transformer.py
@@ -3,7 +3,7 @@ from lark import Transformer as LarkTransformer
 
 from pytest import fixture
 
-from storyscript.parser import Transformer
+from storyscript.parser import Transformer, Tree
 
 
 @fixture
@@ -13,6 +13,13 @@ def transformer():
 
 def test_transformer():
     assert issubclass(Transformer, LarkTransformer)
+
+
+def test_transformer_start(patch, transformer):
+    patch.init(Tree)
+    result = transformer.start(['matches'])
+    Tree.__init__.assert_called_with('line', ['matches'])
+    assert isinstance(result, Tree)
 
 
 def test_transformer_string(transformer):
