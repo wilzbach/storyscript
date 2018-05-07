3fa5df5cdc023b9be98e1a50d78c3173d6ed7a67
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-05-08 00:24:31 +0100

    feat(compiler): add Compiler.command method

--- storyscript/compiler.py
+++ storyscript/compiler.py
@@ -8,6 +8,19 @@ class Compiler:
     Compiles Storyscript abstract syntax tree to JSON.
     """
 
+    @staticmethod
+    def command(tree):
+        dictionary = {
+            'method': 'run',
+            'ln': tree.children[0].line,
+            'container': tree.children[0].value,
+            'args': None,
+            'output': None,
+            'enter': None,
+            'exit': None
+        }
+        return dictionary
+
     @classmethod
     def parse_tree(cls, tree):
         return {}
--- tests/unittests/compiler.py
+++ tests/unittests/compiler.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from lark.lexer import Token
+
 from storyscript.compiler import Compiler
+from storyscript.parser import Tree
 from storyscript.version import version
 
 
@@ -12,3 +15,13 @@ def test_compiler_compile(patch):
 
 def test_compiler_parse_tree():
     assert Compiler.parse_tree('tree') == {}
+
+
+def test_compiler_command():
+    """
+    Ensures that command trees can be compiled
+    """
+    tree = Tree('command', [Token('WORD', 'alpine', line=1)])
+    expected = {'method': 'run', 'ln': 1, 'container': 'alpine', 'args': None,
+                'output': None, 'enter': None, 'exit': None}
+    assert Compiler.command(tree) == expected
