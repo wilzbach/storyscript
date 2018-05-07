0ca6bff20c1cf12d428947fa54f2e8eafd006d0a
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-05-08 00:06:52 +0100

    feat(compiler): add Compiler.parse_tree method

--- storyscript/compiler.py
+++ storyscript/compiler.py
@@ -8,7 +8,11 @@ class Compiler:
     Compiles Storyscript abstract syntax tree to JSON.
     """
 
+    @classmethod
+    def parse_tree(cls, tree):
+        return {}
+
     @staticmethod
     def compile(tree):
-        dictionary = {'script': {}, 'version': version}
+        dictionary = {'script': Compiler.parse_tree(tree), 'version': version}
         return dictionary
--- tests/unittests/compiler.py
+++ tests/unittests/compiler.py
@@ -3,5 +3,12 @@ from storyscript.compiler import Compiler
 from storyscript.version import version
 
 
-def test_compiler_compiler():
-    assert Compiler.compile('tree') == {'script': {}, 'version': version}
+def test_compiler_compile(patch):
+    patch.object(Compiler, 'parse_tree')
+    result = Compiler.compile('tree')
+    Compiler.parse_tree.assert_called_with('tree')
+    assert result == {'script': Compiler.parse_tree(), 'version': version}
+
+
+def test_compiler_parse_tree():
+    assert Compiler.parse_tree('tree') == {}
