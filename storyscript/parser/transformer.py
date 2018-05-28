efe657ff8fb8fbb2c18513e6a0c9a5deafbc0998
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-05-28 17:24:38 +0100

    feat(transformer): add Transformer.service method

--- storyscript/parser/transformer.py
+++ storyscript/parser/transformer.py
@@ -4,9 +4,16 @@ import re
 from lark import Transformer as LarkTransformer
 
 from .tree import Tree
+from ..exceptions import StoryscriptSyntaxError
 
 
 class Transformer(LarkTransformer):
+
+    def service(self, matches):
+        if len(matches[0].children) > 1:
+            token = matches[0].children[1].children[0]
+            raise StoryscriptSyntaxError(1, token)
+        return Tree('service', matches)
 
     def __getattr__(self, attribute, *args):
         return lambda matches: Tree(attribute, matches)
--- tests/unittests/parser/transformer.py
+++ tests/unittests/parser/transformer.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from lark import Transformer as LarkTransformer
 
-from pytest import mark
+from pytest import mark, raises
 
+from storyscript.exceptions import StoryscriptSyntaxError
 from storyscript.parser import Transformer, Tree
 
 
@@ -10,7 +11,18 @@ def test_transformer():
     assert issubclass(Transformer, LarkTransformer)
 
 
-@mark.parametrize('rule', ['start', 'line', 'block', 'command', 'statements'])
+def test_transformer_service(magic):
+    matches = [magic()]
+    assert Transformer().service(matches) == Tree('service', matches)
+
+
+def test_transformer_service_error(magic):
+    matches = [magic(children=[magic(), magic()])]
+    with raises(StoryscriptSyntaxError):
+        Transformer().service(matches)
+
+
+@mark.parametrize('rule', ['start', 'line', 'block', 'command', 'statement'])
 def test_transformer_rules(rule):
     transformer = Transformer()
     result = getattr(transformer, rule)(['matches'])
