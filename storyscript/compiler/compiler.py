ad0354e796b5e5fd4da1dbd11016feebec0333a1
Jacopo <jacopocascioli@gmail.com>
2018-07-09 16:03:47 +0100

    feat(compiler): Compiler.compile uses Preprocessor.process

--- storyscript/compiler/compiler.py
+++ storyscript/compiler/compiler.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from .lines import Lines
 from .objects import Objects
+from .preprocessor import Preprocessor
 from ..exceptions import StoryscriptSyntaxError
 from ..parser import Tree
 from ..version import version
@@ -176,6 +177,7 @@ class Compiler:
 
     @classmethod
     def compile(cls, tree, debug=False):
+        tree = Preprocessor.process(tree)
         compiler = cls.compiler()
         compiler.parse_tree(tree)
         services = compiler.lines.get_services()
