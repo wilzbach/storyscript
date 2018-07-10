8b3fc00109c911e87cabf115db3a2ba3a76adc42
Jacopo <jacopocascioli@gmail.com>
2018-07-10 19:19:27 +0100

    feat(story): add Story.from_file function

--- storyscript/story.py
+++ storyscript/story.py
@@ -6,3 +6,10 @@ class Story:
     def __init__(self, source, source_type):
         self.source = source
         self.source_type = source_type
+
+    @staticmethod
+    def from_file(path):
+        """
+        Creates a story from a file source
+        """
+        return Story(path, 'file')
--- tests/unittests/story.py
+++ tests/unittests/story.py
@@ -12,3 +12,10 @@ def story():
 def test_story_init(story):
     assert story.source == 'source'
     assert story.source_type == 'type'
+
+
+def test_story_from_file(patch):
+    patch.init(Story)
+    result = Story.from_file('hello.story')
+    Story.__init__.assert_called_with('hello.story', 'file')
+    assert isinstance(result, Story)
