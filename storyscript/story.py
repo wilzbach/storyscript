cb048095a9b564736caa11566c12a76839c08734
Jacopo <jacopocascioli@gmail.com>
2018-07-10 19:04:06 +0100

    feat(story): Story.__init__ accepts a source type argument

--- storyscript/story.py
+++ storyscript/story.py
@@ -3,5 +3,6 @@
 
 class Story:
 
-    def __init__(self, source):
+    def __init__(self, source, source_type):
         self.source = source
+        self.source_type = source_type
--- tests/unittests/story.py
+++ tests/unittests/story.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
+from pytest import fixture
+
 from storyscript.story import Story
 
 
-def test_story_init():
-    story = Story('source')
+@fixture
+def story():
+    return Story('source', 'type')
+
+
+def test_story_init(story):
     assert story.source == 'source'
+    assert story.source_type == 'type'
