65c9af6cf42927879b13bfc576353b01b10a8925
Jacopo <jacopocascioli@gmail.com>
2018-07-16 23:30:35 +0100

    feat(app): use Bundle

--- storyscript/app.py
+++ storyscript/app.py
@@ -2,6 +2,7 @@
 import json
 import os
 
+from .bundle import Bundle
 from .parser import Grammar
 from .story import Story
 
@@ -11,52 +12,20 @@ class App:
     Exposes functionalities for internal use e.g the command line
     """
 
-    @staticmethod
-    def get_stories(storypath):
-        """
-        Gets stories from a path
-        """
-        stories = []
-        if os.path.isdir(storypath):
-            for root, subdirs, files in os.walk(storypath):
-                for file in files:
-                    if file.endswith('.story'):
-                        stories.append(os.path.join(root, file))
-            return stories
-        return [storypath]
-
-    @staticmethod
-    def services(stories):
-        """
-        Builds a global list of services from each story's services
-        """
-        services = []
-        for storypath, story in stories.items():
-            services += story['services']
-        services = list(set(services))
-        services.sort()
-        return services
-
     @classmethod
     def compile(cls, path, ebnf_file=None, debug=False):
         """
         Parse and compile stories in path to JSON
         """
-        compiled_stories = {}
-        kwargs = {'ebnf_file': ebnf_file, 'debug': debug}
-        for file in cls.get_stories(path):
-            story = Story.from_file(file)
-            compiled_stories[file] = story.process(**kwargs)
-        services = cls.services(compiled_stories)
-        dictionary = {'stories': compiled_stories, 'services': services}
-        return json.dumps(dictionary, indent=2)
+        bundle = Bundle(path).bundle(ebnf_file=ebnf_file, debug=debug)
+        return json.dumps(bundle, indent=2)
 
     @classmethod
     def lex(cls, path):
         """
         Lex stories, producing the list of used tokens
         """
-        stories = cls.get_stories(path)
+        stories = Bundle(path).find_stories()
         results = {}
         for story in stories:
             results[story] = Story.from_file(story).lex()
--- tests/unittests/app.py
+++ tests/unittests/app.py
@@ -3,76 +3,42 @@ import json
 import os
 
 from storyscript.app import App
+from storyscript.bundle import Bundle
 from storyscript.parser import Grammar
 from storyscript.story import Story
 
 
-def test_app_get_stories(patch):
-    """
-    Ensures App.get_stories returns the original path if it's not a directory
-    """
-    patch.object(os.path, 'isdir', return_value=False)
-    assert App.get_stories('stories') == ['stories']
-
-
-def test_app_get_stories_directory(patch):
-    """
-    Ensures App.get_stories returns stories in a directory
-    """
-    patch.object(os.path, 'isdir')
-    patch.object(os, 'walk', return_value=[('root', [], ['one.story', 'two'])])
-    assert App.get_stories('stories') == ['root/one.story']
-
-
-def test_app_services():
-    compiled_stories = {'a': {'services': ['one']}, 'b': {'services': ['two']}}
-    result = App.services(compiled_stories)
-    assert result == ['one', 'two']
-
-
-def test_app_services_no_duplicates():
-    compiled_stories = {'a': {'services': ['one']}, 'b': {'services': ['one']}}
-    result = App.services(compiled_stories)
-    assert result == ['one']
-
-
 def test_app_compile(patch):
     patch.object(json, 'dumps')
-    patch.object(Story, 'from_file')
-    patch.many(App, ['get_stories', 'services'])
-    App.get_stories.return_value = ['one.story']
+    patch.init(Bundle)
+    patch.object(Bundle, 'bundle')
     result = App.compile('path')
-    App.get_stories.assert_called_with('path')
-    Story.from_file.assert_called_with('one.story')
-    Story.from_file().process.assert_called_with(ebnf_file=None, debug=False)
-    App.services.assert_called_with({'one.story': Story.from_file().process()})
-    dictionary = {'stories': {'one.story': Story.from_file().process()},
-                  'services': App.services()}
-    json.dumps.assert_called_with(dictionary, indent=2)
+    Bundle.__init__.assert_called_with('path')
+    Bundle.bundle.assert_called_with(ebnf_file=None, debug=False)
+    json.dumps.assert_called_with(Bundle.bundle(), indent=2)
     assert result == json.dumps()
 
 
 def test_app_compile_ebnf_file(patch):
     patch.object(json, 'dumps')
-    patch.object(Story, 'from_file')
-    patch.many(App, ['get_stories', 'services'])
-    App.get_stories.return_value = ['one.story']
+    patch.init(Bundle)
+    patch.object(Bundle, 'bundle')
     App.compile('path', ebnf_file='ebnf')
-    Story.from_file().process.assert_called_with(ebnf_file='ebnf', debug=False)
+    Bundle.bundle.assert_called_with(ebnf_file='ebnf', debug=False)
 
 
 def test_app_compile_debug(patch):
     patch.object(json, 'dumps')
-    patch.object(Story, 'from_file')
-    patch.many(App, ['get_stories', 'services'])
-    App.get_stories.return_value = ['one.story']
+    patch.init(Bundle)
+    patch.object(Bundle, 'bundle')
     App.compile('path', debug='debug')
-    Story.from_file().process.assert_called_with(ebnf_file=None, debug='debug')
+    Bundle.bundle.assert_called_with(ebnf_file=None, debug='debug')
 
 
 def test_app_lexer(patch):
     patch.object(Story, 'from_file')
-    patch.object(App, 'get_stories', return_value=['one.story'])
+    patch.init(Bundle)
+    patch.object(Bundle, 'find_stories', return_value=['one.story'])
     result = App.lex('/path')
     Story.from_file.assert_called_with('one.story')
     assert result == {'one.story': Story.from_file().lex()}
