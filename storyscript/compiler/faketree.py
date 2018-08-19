27baada44c92ef8e11c1f99b859b2c9d7e57dc2d
Jacopo <jacopocascioli@gmail.com>
2018-08-20 00:20:08 +0100

    feat(faketree): add FakeTree.path function

--- storyscript/compiler/faketree.py
+++ storyscript/compiler/faketree.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import uuid
 
+from lark.lexer import Token
+
+from ..parser import Tree
+
 
 class FakeTree:
     """
@@ -16,3 +20,11 @@ class FakeTree:
         base = int(line) - 1
         extension = str(uuid.uuid4().int)[:8]
         return '{}.{}'.format(base, extension)
+
+    @staticmethod
+    def path(line):
+        """
+        Creates a virtual path tree.
+        """
+        path = '${}'.format(uuid.uuid4().hex[:8])
+        return Tree('path', [Token('NAME', path, line=line)])
--- tests/unittests/compiler/faketree.py
+++ tests/unittests/compiler/faketree.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import uuid
 
+from lark.lexer import Token
+
 from storyscript.compiler import FakeTree
+from storyscript.parser import Tree
 
 
 def test_faketree_line(patch, magic, tree):
@@ -10,3 +13,10 @@ def test_faketree_line(patch, magic, tree):
     """
     patch.object(uuid, 'uuid4', return_value=magic(int=123456789))
     assert FakeTree.line('1') == '0.12345678'
+
+
+def test_faketree_path(patch):
+    patch.object(uuid, 'uuid4')
+    result = FakeTree.path(1)
+    name = '${}'.format(uuid.uuid4().hex[:8])
+    assert result == Tree('path', [Token('NAME', name, line=1)])
