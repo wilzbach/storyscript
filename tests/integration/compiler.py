5793fd0330d9cbc993bfd02004a2b1a05f6c3385
Jacopo <jacopocascioli@gmail.com>
2018-09-18 00:13:14 +0100

    tests(compiler): add integration tests for functions

--- tests/integration/compiler.py
+++ tests/integration/compiler.py
@@ -16,7 +16,8 @@ def test_compiler_expression_sum(parser):
     """
     Ensures that numbers are compiled correctly
     """
-    result = Compiler.compile(parser.parse('3 + 2'))
+    tree = parser.parse('3 + 2')
+    result = Compiler.compile(tree)
     args = [
         {'$OBJECT': 'expression', 'expression': '{} + {}', 'values': [3, 2]}
     ]
@@ -28,7 +29,8 @@ def test_compiler_expression_mutation(parser):
     """
     Ensures that mutation expressions are compiled correctly
     """
-    result = Compiler.compile(parser.parse("'hello' length"))
+    tree = parser.parse("'hello' length")
+    result = Compiler.compile(tree)
     args = [
         {'$OBJECT': 'string', 'string': 'hello'},
         {'$OBJECT': 'mutation', 'mutation': 'length', 'arguments': []}
@@ -41,7 +43,8 @@ def test_compiler_set(parser):
     """
     Ensures that assignments are compiled correctly
     """
-    result = Compiler.compile(parser.parse('a = 0'))
+    tree = parser.parse('a = 0')
+    result = Compiler.compile(tree)
     assert result['tree']['1']['method'] == 'set'
     assert result['tree']['1']['args'] == [0]
 
@@ -50,7 +53,8 @@ def test_compiler_set_service(parser):
     """
     Ensures that service assignments are compiled correctly
     """
-    result = Compiler.compile(parser.parse('a = alpine echo'))
+    tree = parser.parse('a = alpine echo')
+    result = Compiler.compile(tree)
     assert result['tree']['1']['method'] == 'execute'
     assert result['tree']['1']['name'] == ['a']
 
@@ -59,7 +63,8 @@ def test_compiler_service(parser):
     """
     Ensures that services are compiled correctly
     """
-    result = Compiler.compile(parser.parse("alpine echo message:'hello'"))
+    tree = parser.parse("alpine echo message:'hello'")
+    result = Compiler.compile(tree)
     args = [
         {'$OBJECT': 'argument', 'name': 'message', 'argument':
          {'$OBJECT': 'string', 'string': 'hello'}}
@@ -68,3 +73,30 @@ def test_compiler_service(parser):
     assert result['tree']['1']['service'] == 'alpine'
     assert result['tree']['1']['command'] == 'echo'
     assert result['tree']['1']['args'] == args
+
+
+def test_compiler_function(parser):
+    tree = parser.parse('function sum a:int returns int\n\treturn 0')
+    result = Compiler.compile(tree)
+    args = [{
+        '$OBJECT': 'argument',
+        'argument': {'$OBJECT': 'type', 'type': 'int'}, 'name': 'a'
+    }]
+    assert result['tree']['1']['method'] == 'function'
+    assert result['tree']['1']['function'] == 'sum'
+    assert result['tree']['1']['args'] == args
+    assert result['tree']['1']['output'] == ['int']
+    assert result['tree']['1']['next'] == '2'
+    assert result['tree']['2']['method'] == 'return'
+    assert result['tree']['2']['args'] == [0]
+    assert result['tree']['2']['parent'] == '1'
+
+
+def test_compiler_function_call(parser):
+    source = 'function sum a:int returns int\n\treturn 0\nsum a:1'
+    tree = parser.parse(source)
+    result = Compiler.compile(tree)
+    args = [{'$OBJECT': 'argument', 'name': 'a', 'argument': 1}]
+    assert result['tree']['3']['method'] == 'call'
+    assert result['tree']['3']['service'] == 'sum'
+    assert result['tree']['3']['args'] == args
