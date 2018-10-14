25be9d2fb4db2ca38b17fbcd46b46d501ddc8c7d
Jacopo <jacopocascioli@gmail.com>
2018-10-15 01:12:50 +0200

    feat(transformer): add Transformer.service_block method

--- tests/unittests/parser/transformer.py
+++ tests/unittests/parser/transformer.py
@@ -38,6 +38,35 @@ def test_transformer_assignment_error_dash(magic):
         Transformer().assignment(matches)
 
 
+def test_transformer_service_block():
+    """
+    Ensures service_block nodes are transformed correctly
+    """
+    assert Transformer().service_block([]) == Tree('service_block', [])
+
+
+def test_transformer_service_block_when(tree):
+    """
+    Ensures service_block nodes with a when block are transformed correctly
+    """
+    tree.block.rules = None
+    matches = ['service_block', tree]
+    result = Transformer().service_block(matches)
+    assert result == Tree('service_block', matches)
+
+
+def test_transformer_service_block_indented_arguments(tree, magic):
+    """
+    Ensures service_block with indented arguments are transformed correctly.
+    """
+    tree.find_data.return_value = ['argument']
+    block = magic()
+    matches = [block, tree]
+    result = Transformer().service_block(matches)
+    block.service_fragment.children.append.assert_called_with('argument')
+    assert result == Tree('service_block', [block])
+
+
 @mark.parametrize('rule', ['start', 'line', 'block', 'command', 'statement'])
 def test_transformer_rules(rule):
     transformer = Transformer()
