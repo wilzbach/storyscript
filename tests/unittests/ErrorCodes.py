c58b9ae2abd109385188349dac083cdb33c27b0e
Jacopo Cascioli <jacopo@jacopocascioli.com>
2018-11-17 00:15:19 +0000

    feat(errrorcodes): add more error codes

--- tests/unittests/ErrorCodes.py
+++ tests/unittests/ErrorCodes.py
@@ -1,23 +1,36 @@
 # -*- coding: utf-8 -*-
+from pytest import mark
+
 from storyscript.ErrorCodes import ErrorCodes
 
 
-def test_errorcodes():
-    codes = ErrorCodes
-    assert codes.unidentified_error == ('E0001', '')
-    assert codes.service_name == ('E0002', "A service name can't contain `.`")
-
-    error_3 = ('E0003', 'You have defined an argument, but not a service')
-    assert codes.arguments_noservice == error_3
-
-    error_4 = ('E0004', '`return` is allowed only inside functions')
-    assert codes.return_outside == error_4
-
-    error_5 = ('E0005', "A variable name can't contain `/`")
-    assert codes.variables_backslash == error_5
-
-    error_6 = ('E0006', "A variable name can't contain `-`")
-    assert codes.variables_dash == error_6
-
-    assert codes.incomplete_assignment == ('E0007', 'Missing value after `=`')
-    assert codes.misspelt_function == ('E0008', 'You have misspelt `function`')
+@mark.parametrize('name, definition', [
+    ('unidentified_error', ('E0001', '')),
+    ('service_name', ('E0002', "A service name can't contain `.`")),
+    ('arguments_noservice', (
+        'E0003', 'You have defined an argument, but not a service'
+    )),
+    ('return_outside', ('E0004', '`return` is allowed only inside functions')),
+    ('variables_backslash', ('E0005', "A variable name can't contain `/`")),
+    ('variables_dash', ('E0006', "A variable name can't contain `-`")),
+    ('assignment_incomplete', ('E0007', 'Missing value after `=`')),
+    ('function_misspell', ('E0008', 'You have misspelt `function`')),
+    ('import_misspell', ('E0009', 'You have misspelt `import`')),
+    ('import_misspell_as', ('E0010',
+                            'You have misspelt `as` in an import statement')),
+    ('import_unquoted_file', ('E0011',
+                              'The imported filename must be in quotes')),
+    ('string_opening_quote', ('E0012', 'Missing opening quote for string')),
+    ('string_closing_quote', ('E0013', 'Missing closing quote for string')),
+    ('list_trailing_comma', ('E0014', 'Trailing comma in list')),
+    ('list_opening_bracket', ('E0015', 'Missing opening bracket for list')),
+    ('list_closing_bracket', ('E0016', 'Missing closing bracket for list')),
+    ('object_opening_bracket',  (
+        'E0017', 'Missing opening bracket for object'
+    )),
+    ('object_closing_bracket', ('E0018',
+                                'Missing closing bracket for object')),
+    ('service_argument_colon', ('E0019', 'Missing colon in service argument'))
+])
+def test_errorcodes(name, definition):
+    assert getattr(ErrorCodes, name) == definition
