6dd895aadf97bc86a6db9c770599db99ccbef22a
Sebastian Wilzbach <seb@wilzba.ch>
2018-11-29 20:54:41 +0100

    feat(transformer): add error code for 'raise' keyword

--- tests/unittests/ErrorCodes.py
+++ tests/unittests/ErrorCodes.py
@@ -45,20 +45,21 @@ from storyscript.ErrorCodes import ErrorCodes
     ('reserved_keyword_as', ('E0030', '`as` is a reserved keyword')),
     ('reserved_keyword_import', ('E0031', '`import` is a reserved keyword')),
     ('reserved_keyword_while', ('E0032', '`while` is a reserved keyword')),
+    ('reserved_keyword_raise', ('E0033', '`raise` is a reserved keyword')),
     ('future_reserved_keyword_async', (
-        'E0033',
+        'E0034',
         '`async` is reserved for future use')),
     ('future_reserved_keyword_story', (
-        'E0034',
+        'E0035',
         '`story` is reserved for future use')),
     ('future_reserved_keyword_assert', (
-        'E0035',
+        'E0036',
         '`assert` is reserved for future use')),
     ('future_reserved_keyword_called', (
-        'E0036',
+        'E0037',
         '`called` is reserved for future use')),
     ('future_reserved_keyword_mock', (
-        'E0037',
+        'E0038',
         '`mock` is reserved for future use')),
 ])
 def test_errorcodes(name, definition):
