e9ed8ad9021ef65c6b259c1c97babd4e06afbeb4
Sebastian Wilzbach <seb@wilzba.ch>
2019-01-17 16:26:49 +0100

    feat(CompilerError): allow to store extra arguments in the CompilerError

--- tests/unittests/exceptions/CompilerError.py
+++ tests/unittests/exceptions/CompilerError.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from pytest import raises
+
 from storyscript.ErrorCodes import ErrorCodes
-from storyscript.exceptions import CompilerError, ProcessingError
+from storyscript.exceptions import ProcessingError
+from storyscript.exceptions.CompilerError import CompilerError, ConstDict
 
 
 def test_compilererror():
@@ -20,3 +23,17 @@ def test_error_message(patch):
     ErrorCodes.is_error.return_value = False
     e3 = CompilerError(None)
     assert str(e3) == 'Unknown compiler error'
+
+
+def test_const_dict():
+    d = ConstDict({'foo': 'bar', 'f2': 'b2'})
+    assert d.foo == 'bar'
+    assert d.f2 == 'b2'
+    with raises(Exception):
+        d.bar
+
+
+def test_compiler_error_extra_parameters():
+    e2 = CompilerError('my_custom_error', my_param='p1', my_param2='p2')
+    assert e2.extra.my_param == 'p1'
+    assert e2.extra.my_param2 == 'p2'
