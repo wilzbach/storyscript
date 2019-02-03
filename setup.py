5df88f24e4df4353ea6817533f5ed0051bd0a5fa
Sebastian Wilzbach <seb@wilzba.ch>
2019-02-03 17:46:38 +0100

    feat(setup): automatically detect and provide the current version

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import io
+from os import path
 
 from setuptools import find_packages, setup
+from setuptools.command.install import install as _install
+from setuptools.command.sdist import sdist as _sdist
+
+root_dir = path.dirname(__file__)
 
 
-version = '0.9.5'
+# Read a file and return its as a string
+def read(file_name):
+    return io.open(path.join(root_dir, file_name)).read()
 
-description = io.open('README.md', 'r', encoding='utf-8').read()
+
+name = 'storyscript'
+version = None
+release_version = None
+# try loading the current version
+try:
+    result = {'__file__': path.join(root_dir, name, 'Version.py')}
+    exec(read(path.join(name, 'Version.py')), result)
+    version = result['version']
+    release_version = result['release_version']
+except FileNotFoundError:
+    pass
+
+description = read('README.md')
 short_description = ('StoryScript is an high-level language that can be used '
                      'to orchestrate microservices in an algorithmic way.')
 
@@ -35,14 +56,44 @@ extras = [
     'guzzle-sphinx-theme'
 ]
 
+###############################################################################
+# Custom build steps
+###############################################################################
 
-setup(name='storyscript',
-      version=version,
+
+def prepare_release(cwd, use_release):
+    version_file = path.join(cwd, name, 'VERSION')
+    if use_release:
+        version_text = release_version
+    else:
+        version_text = version
+    print(f'writing version({version_text}) -> {version_file}')
+    with open(version_file, 'w') as f:
+        f.write(version_text)
+
+
+class Install(_install):
+    def run(self):
+        _install.run(self)
+        self.execute(prepare_release, (self.install_lib, False),
+                     msg='Preparing the installation')
+
+
+class Sdist(_sdist):
+    def make_release_tree(self, basedir, files):
+        _sdist.make_release_tree(self, basedir, files)
+        self.execute(prepare_release, (basedir, True),
+                     msg='Building the release')
+
+
+setup(name=name,
+      version=release_version,
       description=short_description,
       long_description=description,
       long_description_content_type='text/markdown',
       classifiers=classifiers,
-      download_url='https://github.com/asyncy/storyscript/archive/master.zip',
+      download_url=('https://github.com/asyncy/storyscript/archive/'
+                    f'{version}.zip'),
       keywords='',
       author='Asyncy',
       author_email='noreply@storyscript.org',
@@ -57,4 +108,8 @@ setup(name='storyscript',
       },
       entry_points={
           'console_scripts': ['storyscript=storyscript.Cli:Cli.main']
+      },
+      cmdclass={
+        'install': Install,
+        'sdist': Sdist,
       })
--- storyscript/Version.py
+++ storyscript/Version.py
@@ -1,2 +1,70 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-version = '0.9.5'
+import io
+import subprocess
+from os import path
+
+root_dir = path.abspath(path.dirname(path.dirname(__file__)))
+
+
+def git_version():
+    return subprocess.run(
+        ['git', 'describe', '--abbrev=0', '--tags'],
+        stdout=subprocess.PIPE,
+        text=True,
+        check=True,
+        cwd=root_dir,
+    ).stdout.strip()
+
+
+def git_describe():
+    return subprocess.run(
+        ['git', 'describe', '--dirty'],
+        stdout=subprocess.PIPE,
+        text=True,
+        check=True,
+        cwd=root_dir,
+    ).stdout.strip()
+
+
+def read_version():
+    return io.open(path.join(root_dir, 'storyscript', 'VERSION'), 'r',
+                   encoding='utf8').read().strip()
+
+
+def get_version():
+    # try detecting the git version first
+    try:
+        return git_describe()
+    except Exception:
+        pass
+
+    # fallback to a VERSION file (e.g. for a released storyscript)
+    try:
+        return read_version()
+    except Exception:
+        pass
+
+    # soft fallback in case everything fails
+    return '0.0.0'
+
+
+def get_release_version():
+    # try detecting the git version first
+    try:
+        return git_version()
+    except Exception:
+        pass
+
+    # fallback to a VERSION file (e.g. for a released storyscript)
+    try:
+        return read_version()
+    except Exception:
+        pass
+
+    # soft fallback in case everything fails
+    return '0.0.0'
+
+
+version = get_version()
+release_version = get_release_version()
