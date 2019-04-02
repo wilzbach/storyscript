e6ae7f8be268af9e70d3de9ffb66b4f3e1e25ba5
Sebastian Wilzbach <seb@wilzba.ch>
2019-04-03 01:00:22 +0200

    refactor(compiler): use a static type dict in JSONExpressionVisitor

--- storyscript/compiler/json/JSONExpressionVisitor.py
+++ storyscript/compiler/json/JSONExpressionVisitor.py
@@ -10,18 +10,22 @@ class JSONExpressionVisitor(ExpressionVisitor):
     def __init__(self, visitor):
         self.visitor = visitor
 
+    _types = {
+        'PLUS': 'sum', 'DASH': 'subtraction', 'POWER': 'exponential',
+        'MULTIPLIER': 'multiplication', 'BSLASH': 'division',
+        'MODULUS': 'modulus',
+        'AND': 'and', 'OR': 'or', 'NOT': 'not', 'EQUAL': 'equals',
+        'GREATER': 'greater', 'LESSER': 'less',
+        'NOT_EQUAL': 'not_equal',
+        'GREATER_EQUAL': 'greater_equal',
+        'LESSER_EQUAL': 'less_equal'
+    }
+
     def expression_type(self, operator, tree):
-        types = {'PLUS': 'sum', 'DASH': 'subtraction', 'POWER': 'exponential',
-                 'MULTIPLIER': 'multiplication', 'BSLASH': 'division',
-                 'MODULUS': 'modulus',
-                 'AND': 'and', 'OR': 'or', 'NOT': 'not', 'EQUAL': 'equals',
-                 'GREATER': 'greater', 'LESSER': 'less',
-                 'NOT_EQUAL': 'not_equal',
-                 'GREATER_EQUAL': 'greater_equal',
-                 'LESSER_EQUAL': 'less_equal'}
-        tree.expect(operator in types, 'compiler_error_no_operator',
+        ret = self._types.get(operator, None)
+        tree.expect(ret is not None, 'compiler_error_no_operator',
                     operator=operator)
-        return types[operator]
+        return ret
 
     def values(self, tree):
         return self.visitor.values(tree)
