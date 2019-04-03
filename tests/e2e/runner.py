a9e08478545c660fc78ba6393229e4051c5d1882
Sebastian Wilzbach <seb@wilzba.ch>
2019-04-03 03:33:29 +0200

    fix(runnner): use recursive globbing in the e2e test runner

--- tests/e2e/runner.py
+++ tests/e2e/runner.py
@@ -16,7 +16,7 @@ from storyscript.exceptions import StoryError
 test_dir = path.dirname(path.realpath(__file__))
 # make the test_file paths relative, s.t. the tests are nice to read
 test_files = list(map(lambda e: path.relpath(e, test_dir),
-                  glob(path.join(test_dir, '*.story'))))
+                  glob(path.join(test_dir, '**', '*.story'), recursive=True)))
 
 
 # compile a story and compare its tree with the expected tree
