9161c683fa2ec7e4f34de2e857627750a31bddab
Anukul Sangwan <anukulsangwan@icloud.com>
2019-07-16 14:13:58 +0200

    Add approxEqual variants

--- storyscript/compiler/semantics/functions/HubMutations.py
+++ storyscript/compiler/semantics/functions/HubMutations.py
@@ -65,6 +65,8 @@ float abs -> float
 float isNaN -> boolean
 float isInfinity -> boolean
 float approxEqual value: float -> boolean
+float approxEqual value: float maxRelDiff: float -> boolean
+float approxEqual value: float maxAbsDiff: float -> boolean
 float approxEqual value: float maxRelDiff: float maxAbsDiff: float -> boolean
 float sqrt -> float
 """
