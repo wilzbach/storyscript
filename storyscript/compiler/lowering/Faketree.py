7f4090663e58d2ef7029274966474e189052c9fd
Aditya Bansal <adi.bansal241996@gmail.com>
2019-08-19 17:51:23 +0530

    fix(FakeTree): check token is not None before accessing line

--- storyscript/compiler/lowering/Faketree.py
+++ storyscript/compiler/lowering/Faketree.py
@@ -74,6 +74,7 @@ class FakeTree:
         """
         line = self.get_line(value)
         first_token = value.find_first_token()
+        assert first_token is not None
         first_token.line = line
         path = self.path(line=line)
         return self.assignment_path(path, value, line)
